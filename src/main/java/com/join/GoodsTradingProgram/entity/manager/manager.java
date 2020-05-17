8b1c9fa3cfee247bea8325bb2ea5014589c09ec3
liulq <1125052258@qq.com>
2020-05-17 14:22:21 +0800

    first commit

---
+++ src/main/java/com/join/GoodsTradingProgram/entity/manager/manager.java
@@ -0,0 +1,37 @@
+package com.join.GoodsTradingProgram.entity.manager;
+
+/**
+ * @Author: Liulq
+ * @Description:
+ * @Date: 21:30 2020/5/16
+ */
+public class manager {
+
+    private int id;
+    private String managerName;
+    private String password;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getManagerName() {
+        return managerName;
+    }
+
+    public void setManagerName(String managerName) {
+        this.managerName = managerName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
