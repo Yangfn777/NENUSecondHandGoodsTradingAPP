9512156f6ee3295a8fdad56146c08cee04fef1fe
liulq <1125052258@qq.com>
2020-05-23 23:31:43 +0800

    管理员操作完善

---
+++ src/test/java/com/join/GoodsTradingProgram/test/ManagerTest.java
@@ -0,0 +1,17 @@
+package com.join.GoodsTradingProgram.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author: Liulq
+ * @Description:
+ * @Date: 23:10 2020/5/23
+ */
+@SpringBootTest
+public class ManagerTest {
+    @Test
+    public void ListAllTest(){
+
+    }
+}
