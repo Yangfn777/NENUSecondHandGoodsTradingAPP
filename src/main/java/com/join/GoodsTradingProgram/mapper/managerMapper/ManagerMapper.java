315f4fc02c02ca556c74aa6d050e60d7ffe95ae7
liulq <1125052258@qq.com>
2020-05-23 23:30:18 +0800

    管理员操作完善

--- src/main/java/com/join/GoodsTradingProgram/mapper/managerMapper/ManagerMapper.java
+++ src/main/java/com/join/GoodsTradingProgram/mapper/managerMapper/ManagerMapper.java
@@ -1,6 +1,7 @@
 package com.join.GoodsTradingProgram.mapper.managerMapper;
 
 import com.join.GoodsTradingProgram.entity.manager.Manager;
+import com.join.GoodsTradingProgram.entity.user.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
@@ -24,9 +25,40 @@ public interface ManagerMapper {
     public Manager login(@Param("manager")Manager manager)throws Exception;
 
     /**
-     * 列出所有管理者
+     * 增加管理员
+     * @param manager
+     * @return
+     * @throws Exception
+     */
+    public Manager addManager(@Param("manager")Manager manager)throws Exception;
+
+    /**
+     * 修改个人信息
+     * @param managerName
+     * @param password
+     * @return
+     * @throws Exception
+     */
+    public Manager updateManager(@Param("managerName")String managerName,@Param("password")String password)throws Exception;
+    /**
+     * 列出所有管理者(查）
      * @return
      * @throws Exception
      */
     public List<Manager> listManager()throws Exception;
+
+    /**
+     * 查询用户列表
+     * @return
+     * @throws Exception
+     */
+    public List<User> listUser()throws Exception;
+
+    /**
+     * 删除用户
+     * @param user
+     * @return
+     * @throws Exception
+     */
+    public User deleteUser(@Param("user")User user)throws Exception;
 }
