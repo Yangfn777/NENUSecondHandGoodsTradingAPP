495fec58ac867f257343ebd5e60fcf43ead36a63
liulq <1125052258@qq.com>
2020-05-23 23:30:31 +0800

    管理员操作完善

--- src/main/java/com/join/GoodsTradingProgram/service/managerService/impl/ManagerServiceImpl.java
+++ src/main/java/com/join/GoodsTradingProgram/service/managerService/impl/ManagerServiceImpl.java
@@ -1,6 +1,7 @@
 package com.join.GoodsTradingProgram.service.managerService.impl;
 
 import com.join.GoodsTradingProgram.entity.manager.Manager;
+import com.join.GoodsTradingProgram.entity.user.User;
 import com.join.GoodsTradingProgram.mapper.managerMapper.ManagerMapper;
 import com.join.GoodsTradingProgram.service.managerService.ManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,12 +32,56 @@ public class ManagerServiceImpl implements ManagerService {
     }
 
     /**
-     * 管理员列表
+     * 增加管理员
+     * @param manager
      * @return
      * @throws Exception
      */
     @Override
-    public List<Manager> ListAll() throws Exception {
+    public Manager addManager(Manager manager) throws Exception {
+        return null;
+    }
+
+    /**
+     * 修改管理员个人信息
+     * @param managerName
+     * @param password
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public Manager updateManager(String managerName, String password) throws Exception {
+        return null;
+    }
+
+    /**
+     * 查询管理员列表
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public List<Manager> ListManager() throws Exception {
+        return null;
+    }
+
+    /**
+     * 查询用户列表
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public List<User> ListUser() throws Exception {
+        return null;
+    }
+
+    /**
+     * 删除用户
+     * @param user
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public User deleteUser(User user) throws Exception {
         return null;
     }
 }
