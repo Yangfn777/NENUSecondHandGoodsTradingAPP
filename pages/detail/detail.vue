a0fd46b7e4a07067ee95816340c058d499945f1f
WangPolaris <1559114369@qq.com>
2020-06-16 08:53:16 +0800

    wlh

---
+++ pages/detail/detail.vue
@@ -0,0 +1,93 @@
+<template>
+	<view class="">
+		<view class="w-100 d-flex" style="padding:16rpx;border-bottom: 1px solid #BEBEBE;">
+			<view class="" style="width:80%;">
+				<view class="" style="font-size: 18px; font-weight: bold;">{{ good.title }}</view>
+				<view class="">
+					<text class="mr-2">{{ good.time }}</text>
+					<text>浏览人数{{ good.num }}人</text>
+				</view>
+			</view>
+			<view class="" style="width: 20%;">
+				<image src="../../static/images/shoucang.png" mode="widthFix" v-if="star" style="width: 60rpx;"></image>
+				<image src="../../static/images/star-opp.png" mode="widthFix" v-else></image>
+			</view>
+		</view>
+		<view class="" style="padding:16rpx">
+			<view class="">图片详情</view>
+			<view class="uni-padding-wrap w-100 p-0">
+				<view class="page-section swiper">
+					<view class="page-section-spacing">
+						<swiper class="swiper" :indicator-dots="indicatorDots" :autoplay="autoplay" :interval="interval" :duration="duration" circular="true">
+							<swiper-item v-for="(item,index) in good.imgList" :key="index"><image :src="item" mode="heightFix" class="w-100"></image></swiper-item>
+						</swiper>
+					</view>
+				</view>
+			</view>
+		</view>
+		<view class="d-flex w-100 p-1 a-center" style="border-bottom: 1px solid #BEBEBE">
+			<view class="text-center" style="width:24%; font-size:16px;font-weight: 600;">详细描述</view>
+			<view class="" style="width:86%; line-height:1.2 !important; color:#7d7d7d">{{good.des}}</view>
+		</view>
+		<view class="d-flex w-100 p-1 a-center" style="border-bottom: 1px solid #BEBEBE;">
+			<view class="text-center" style="width:24%; font-size:16px;font-weight: 600;">类型标签</view>
+			<view class="" style="width:86%; line-height:1.2 !important; color:#007AFF">#{{good.meta}}</view>
+		</view>
+		<view class="d-flex w-100 p-1 a-center" style="border-bottom: 1px solid #BEBEBE;">
+			<view class="text-center" style="width:24%; font-size:16px;font-weight: 600;">价钱</view>
+			<view class="" style="width:86%; line-height:1.2 !important; color:#7d7d7d">{{good.price}}元</view>
+		</view>
+		<view class="d-flex w-100 p-1 a-center" style="border-bottom: 1px solid #BEBEBE;">
+			<view class="text-center" style="width:24%; font-size:16px;font-weight: 600;">联系人</view>
+			<view class="" style="width:86%; line-height:1.2 !important; color:#7d7d7d">{{good.person}}</view>
+		</view>
+		<view class="d-flex w-100 p-1 a-center" style="border-bottom: 1px solid #BEBEBE;">
+			<view class="text-center" style="width:24%; font-size:16px;font-weight: 600;">手机号</view>
+			<view class="" style="width:86%; line-height:1.2 !important; color:#7d7d7d">{{good.tel}}</view>
+		</view>
+		<view class="" style="border-bottom: 1px solid #BEBEBE;padding:20rpx;background-color: #EEEEEE">
+			为保障用户信息的安全，请您先进行学生注册与认证后即可查看具体联系人信息。
+		</view>
+		<view class="">
+			<view class="" style="height:100rpx;line-height: 100rpx;border-bottom:1px solid #BEBEBE">全部留言(0)</view>
+			<view class="" style="height:180rpx;line-height: 180rpx;border-bottom:1px solid #BEBEBE;text-align: center;color:#BEBEBE">
+				该信息暂时没有留言
+			</view>
+			<view class="d-flex">
+				<input type="text" value="" placeholder="有什么想了解的写出来吧" style="margin-top:20rpx;;border-bottom: 1px solid #BEBEBE;width:70%;height:80rpx;line-height: 80rpx; margin-left:50rpx;">
+				<button type="default" style="width:20%;background-color: #007AFF;color: #FFFFFF;margin-top:20rpx;">发送</button>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			background: ['color1', 'color2', 'color3'],
+			indicatorDots: true,
+			autoplay: true,
+			interval: 2000,
+			duration: 500,
+			star: true,
+			good: {
+				title: '考研复习资料全套',
+				time: '2020-6-6 13:00:00',
+				num: '32',
+				imgList: ['../../static/images/10.jpg','../../static/images/10.jpg'],
+				des: '考研资料几乎全新，考试前花了300百购买，现在考研结束，140出，不邮寄，一食堂自行取货！非同校勿扰，谢谢！',
+				meta: '书本类',
+				price: 140,
+				person: '奔跑',
+				tel: '183****2881'
+			}
+		};
+	},
+	methods: {}
+};
+</script>
+
+<style>
+	@import url("");
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,31 +1,87 @@
 <template>
 	<view class="content">
-		hahahahahhasssss
+		<!-- 基本用法 -->
+		<uni-search-bar @confirm="search" @input="input" class="text-center mt-1 mb-1" style="padding: 0px !important; width:90%"></uni-search-bar>
+		<view class="uni-padding-wrap w-100">
+			<view class="page-section swiper">
+				<view class="page-section-spacing">
+					<swiper class="swiper" indicator-dots="true" autoplay="true" interval="2000" duration="500" circular="true">
+						<swiper-item><image src="../../static/images/1.jpg"></image></swiper-item>
+						<swiper-item><image src="../../static/images/2.jpg"></image></image></swiper-item>
+						<swiper-item><image src="../../static/images/3.jpg"></image></swiper-item>
+						<swiper-item><image src="../../static/images/4.jpg"></image></swiper-item>
+					</swiper>
+				</view>
+			</view>
+		</view>
+		<!-- 商品信息 -->
+		<view class="d-flex w-100" v-for="(item,index) in goods" :key="index" style="border-bottom:1px solid #BEBEBE">
+			<view style="width:20%" class="text-center">
+				<image :src="item.headPic" mode="" style="width: 100rpx;height: 100rpx;border-radius: 100%;"></image>
+			</view>
+			<view style="width:80%">
+				<view class="" style="font-size: 18px; font-weight: 500;">{{item.nick}}</view>
+				<view class="" style="color:#767676">{{item.title}}</view>
+				<view class="d-flex j-sb">
+					<image :src="item.imgList[0]" mode="widthFix" class="mr-1"></image>
+					<image :src="item.imgList[1]" mode="widthFix"></image>
+				</view>
+				<view class="" style="color:#007bff">#{{item.meta}}</view>
+				<view class="mb-1">
+					<text style="margin-right:100rpx;">{{item.time}}</text>
+					<text @click="goDetail" :id="item.id">浏览</text>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				title: 'Hello'
-			}
-		},
-		onLoad() {
-
-		},
-		methods: {
-
+import uniSearchBar from '@/components/uni-search-bar/uni-search-bar.vue';
+export default {
+	components: { uniSearchBar },
+	data() {
+		return {
+			goods:[{
+				id:0,
+				headPic:'../../static/images/5.jpg',
+				nick:'奔跑',
+				title:'出售考研书籍',
+				imgList:['../../static/images/10.jpg','../../static/images/10.jpg'],
+				meta:'书本类',
+				time:'2020-6-6 13:00:00'
+			},
+			{
+				id:1,
+				headPic:'../../static/images/6.jpg',
+				nick:'向上',
+				title:'出售洗面奶和护肤品',
+				imgList:['../../static/images/11.jpg','../../static/images/12.jpg'],
+				meta:'生活用品类',
+				time:'2020-6-6 13:00:00'
+			}]
+		};
+	},
+	onLoad() {},
+	methods: {
+		goDetail(e){
+			//这个商品的id
+			var id=e.currentTarget.id
+			console.log(id);
+			uni.navigateTo({
+				url:"../detail/detail?id="+id
+			})
 		}
 	}
+};
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		color: #4e86ff;
-	}
+.content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	/* color: #4e86ff; */
+}
 </style>
